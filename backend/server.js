15fac204933c6fe32b79d5733bc611a8ad52dfd4
iamshivamgaur <shivam24august@gmail.com>
2025-02-26 16:41:58 +0530

    deployment code also

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -12,12 +13,13 @@ import connectToMongoDb from "./db/connectToMongoDb.js";
 // Importing Socket server
 import { app, server } from "./socket/socket.js";
 
-
 dotenv.config();
 
 // deleting because of socket.io
 // const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse the incomming requests with JSON payloads from (req.body)
 app.use(cookieParser());
@@ -26,6 +28,13 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.send(path.join(__dirname, "/frontend/dist/index.html"));
+});
+
 
 server.listen(PORT, () => {
   connectToMongoDb();
