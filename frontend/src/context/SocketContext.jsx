76048788d786adf261bfb6f74ef9a6bc34818feb
iamshivamgaur <shivam24august@gmail.com>
2025-02-26 17:23:46 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8080", {
+      const socket = io("https://chat-application-socket-io.onrender.com/", {
         query: {
           userId: authUser._id,
         },
